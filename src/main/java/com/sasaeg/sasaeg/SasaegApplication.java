c822a8f04db4184e168713c594fdbd9d68b84f4c
danikaaa <dltndus9563@naver.com>
2023-10-31 22:04:53 +0900

    first commit

---
+++ src/main/java/com/sasaeg/sasaeg/SasaegApplication.java
@@ -0,0 +1,13 @@
+package com.sasaeg.sasaeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasaegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SasaegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sasaeg/sasaeg/SasaegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasaeg.sasaeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasaegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
